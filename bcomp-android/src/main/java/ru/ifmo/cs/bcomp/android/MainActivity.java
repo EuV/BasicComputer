553407680f668d58cd1f91998b407c1c45c59fed
Vasilev Evgenij <vasil.ev.genij@gmail.com>
2015-01-18 00:01:12 +0400

    Add bcomp-android module
    Set up maven and create MainActivity

---
+++ bcomp-android/src/main/java/ru/ifmo/cs/bcomp/android/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.ifmo.cs.bcomp.android;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
