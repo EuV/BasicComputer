6241866fc437a8064ad5a8ea11abefa9f13a2bf6
Vasilev Evgenij <vasil.ev.genij@gmail.com>
2015-01-27 15:56:52 +0400

    Add rough template of the layout

--- bcomp-android/src/main/java/ru/ifmo/cs/bcomp/android/MainActivity.java
+++ bcomp-android/src/main/java/ru/ifmo/cs/bcomp/android/MainActivity.java
@@ -2,6 +2,10 @@ package ru.ifmo.cs.bcomp.android;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.Random;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends ActionBarActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        RecyclerView memoryView = (RecyclerView) findViewById(R.id.memory_recycler_view);
+        memoryView.setHasFixedSize(true);
+        memoryView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        String[] rows = new String[2048];
+        Random random = new Random();
+        for (int i = 0; i < rows.length; i++) {
+            rows[i] = String.format("%03X | %04X", i, random.nextInt(65535));
+        }
+
+        memoryView.setAdapter(new MemoryAdapter(rows));
     }
 }
