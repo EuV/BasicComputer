444e2f274c9faef36f978d3b108e7493185c8db9
Vasilev Evgenij <vasil.ev.genij@gmail.com>
2015-02-15 00:52:51 +0400

    Implement Tabs via ViewPager

--- bcomp-android/src/main/java/ru/ifmo/cs/bcomp/android/MainActivity.java
+++ bcomp-android/src/main/java/ru/ifmo/cs/bcomp/android/MainActivity.java
@@ -1,7 +1,15 @@
 package ru.ifmo.cs.bcomp.android;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity implements KeyboardFragment.OnKeyboardEventListener {
@@ -10,10 +18,49 @@ public class MainActivity extends ActionBarActivity implements KeyboardFragment.
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        setupViewPager();
     }
 
     @Override
     public void onKeyboardPressed() {
         Toast.makeText(this, "Keyboard register pressed!", Toast.LENGTH_SHORT).show();
+    }
+
+
+    protected void setupViewPager() {
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.tab_view_pager);
+        viewPager.setAdapter(new TabAdapter(getSupportFragmentManager()));
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+             return;
+        }
+
+        final View contentStub = findViewById(R.id.tab_content_stub);
+        contentStub.getViewTreeObserver().addOnGlobalLayoutListener(
+            new ViewTreeObserver.OnGlobalLayoutListener() {
+                @Override
+                public void onGlobalLayout() {
+                    ViewGroup.LayoutParams params = viewPager.getLayoutParams();
+                    params.height = contentStub.getHeight();
+                    viewPager.setLayoutParams(params);
+                }
+            });
+    }
+
+
+    protected class TabAdapter extends FragmentStatePagerAdapter {
+        public TabAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int tab) {
+            return new BasicFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
     }
 }
