3473ad60e4090e0307009908505c77ef13eb30df
Vasilev Evgenij <vasil.ev.genij@gmail.com>
2015-02-22 00:51:38 +0400

    Add keyboard hex selector popup

--- bcomp-android/src/main/java/ru/ifmo/cs/bcomp/android/KeyboardFragment.java
+++ bcomp-android/src/main/java/ru/ifmo/cs/bcomp/android/KeyboardFragment.java
@@ -1,11 +1,14 @@
 package ru.ifmo.cs.bcomp.android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class KeyboardFragment extends Fragment {
@@ -14,25 +17,95 @@ public class KeyboardFragment extends Fragment {
         void onKeyboardPressed();
     }
 
+    public static final int HEX_SYMBOL_REQUEST = 0;
+    public static final String HEX_SYMBOL_VALUE = "hex_symbol_value";
+    public static final String HEX_SYMBOL_PRESSED_TAG = "hex_symbol_tag";
+
     private KeyboardCallbacks callbacks;
+    private View keyboardView;
+    private String hexSymbolPressedTag;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View keyboardView = inflater.inflate(R.layout.keyboard_fragment, container, false);
 
-        keyboardView.findViewById(R.id.ok_keyboard_button).setOnClickListener(new View.OnClickListener() {
+        if (savedInstanceState != null) {
+            hexSymbolPressedTag = savedInstanceState.getString(HEX_SYMBOL_PRESSED_TAG);
+        }
+
+        keyboardView = inflater.inflate(R.layout.keyboard_fragment, container, false);
+
+        keyboardView.findViewById(R.id.ok_keyboard_button).setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 callbacks.onKeyboardPressed();
             }
         });
 
+        HexSymbolOnClickListener listener = new HexSymbolOnClickListener();
+        keyboardView.findViewById(R.id.keyboard_hex_0).setOnClickListener(listener);
+        keyboardView.findViewById(R.id.keyboard_hex_1).setOnClickListener(listener);
+        keyboardView.findViewById(R.id.keyboard_hex_2).setOnClickListener(listener);
+        keyboardView.findViewById(R.id.keyboard_hex_3).setOnClickListener(listener);
+
         return keyboardView;
     }
+
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         callbacks = (KeyboardCallbacks) activity;
+    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putString(HEX_SYMBOL_PRESSED_TAG, hexSymbolPressedTag);
+        super.onSaveInstanceState(outState);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (hexSymbolPressedTag == null) return;
+
+        if (requestCode == HEX_SYMBOL_REQUEST && resultCode == Activity.RESULT_OK) {
+
+            View symbolView = null;
+            switch (hexSymbolPressedTag) {
+                case "hex_0":
+                    symbolView = keyboardView.findViewById(R.id.keyboard_hex_0);
+                    break;
+                case "hex_1":
+                    symbolView = keyboardView.findViewById(R.id.keyboard_hex_1);
+                    break;
+                case "hex_2":
+                    symbolView = keyboardView.findViewById(R.id.keyboard_hex_2);
+                    break;
+                case "hex_3":
+                    symbolView = keyboardView.findViewById(R.id.keyboard_hex_3);
+                    break;
+            }
+
+            if (symbolView != null) {
+                ((TextView) symbolView).setText(String.format("%X", data.getIntExtra(HEX_SYMBOL_VALUE, 0)));
+            }
+        }
+
+        hexSymbolPressedTag = null;
+    }
+
+
+    protected class HexSymbolOnClickListener implements OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            if (hexSymbolPressedTag != null) return;
+            hexSymbolPressedTag = (String) view.getTag();
+            Intent intent = new Intent(getActivity(), KeyboardPopupActivity.class);
+            startActivityForResult(intent, HEX_SYMBOL_REQUEST);
+        }
     }
 }
---
+++ bcomp-android/src/main/java/ru/ifmo/cs/bcomp/android/KeyboardPopupActivity.java
@@ -0,0 +1,56 @@
+package ru.ifmo.cs.bcomp.android;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class KeyboardPopupActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        LinearLayout firstRow = new LinearLayout(this);
+        for (int i = 0x0; i <= 0x7; i++) {
+            firstRow.addView(new HexButton(this, i));
+        }
+
+        LinearLayout secondRow = new LinearLayout(this);
+        for (int i = 0x8; i <= 0xF; i++) {
+            secondRow.addView(new HexButton(this, i));
+        }
+
+        LinearLayout mainLayout = new LinearLayout(this);
+        mainLayout.setOrientation(LinearLayout.VERTICAL);
+        mainLayout.addView(firstRow);
+        mainLayout.addView(secondRow);
+        setContentView(mainLayout);
+    }
+
+
+    private class HexButton extends Button {
+        public HexButton(Context context, final int value) {
+            super(context);
+            setMinimumWidth(0);
+            setWidth(70);
+            setPadding(0, 0, 0, 0);
+            setText(String.format("%X", value));
+            setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent resultData = new Intent();
+                    resultData.putExtra(KeyboardFragment.HEX_SYMBOL_VALUE, value);
+                    setResult(Activity.RESULT_OK, resultData);
+                    finish();
+                }
+            });
+        }
+    }
+}
